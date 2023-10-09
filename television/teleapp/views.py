9bd78d42c6900b9a29579dff466f415cfb3b4287
Naveena <navvna22@gmail.com>
2023-10-09 13:18:03 +0530

    added files

---
+++ television/teleapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def naveena1(request):
+    return render(request,'home.html')
+
+def naveena2(request):
+    return render(request,'index.html')
+    
